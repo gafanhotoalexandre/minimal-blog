398d57509f2c7e2632b57b952b4b81b73f03bab6
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-16 18:50:04 -0300

    listando artigos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import { ArticleListItem } from '@/components/ArticleListItem'
+import { getCategorisedArticles } from '@/lib/articles'
+
 export default function Home() {
+  const articles = getCategorisedArticles()
+
+  console.log(articles)
   return (
     <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
       <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
         <h1>dev memories</h1>
       </header>
 
-      <section className="md:grid md:grid-cols-2 flex flex-col gap-10"></section>
+      <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
+        {articles !== null &&
+          Object.keys(articles).map((category) => (
+            <ArticleListItem
+              key={category}
+              category={category}
+              articles={articles[category]}
+            />
+          ))}
+      </section>
     </section>
   )
 }
---
+++ components/ArticleListItem.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+interface ArticleListItemProps {
+  category: string
+  articles: ArticleItem[]
+}
+export function ArticleListItem({ category, articles }: ArticleListItemProps) {
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="font-cormorantGaramond text-4xl">{category}</h2>
+
+      <div className="flex flex-col gap-2.5 font-poppins text-lg">
+        {articles.map((article, index) => (
+          <Link
+            key={index}
+            href={`/${article.id}`}
+            className="text-neutral-900 hover:text-amber-700 transition duration-150"
+          >
+            {article.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
